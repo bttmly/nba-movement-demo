171f4655149e0bdc2cde181f54868314490efb6f
Nick Bottomley <nhbottomley@gmail.com>
2017-01-31 14:05:29 -0800

    whoops, not sure what that first argument to res.json was doing there...

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
 if (app.get("env") === "development") {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.json("error", {
+        res.json({
             message: err.message,
             error: err
         });
@@ -43,7 +43,7 @@ if (app.get("env") === "development") {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.json("error", {
+    res.json({
         message: err.message,
         error: {}
     });
